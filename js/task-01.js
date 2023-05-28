7db0683a818f8beb6718d666c8e1d55b66b739bf
KatanES <businesskatan.es@gmail.com>
2023-05-28 12:53:05 +0200

    1st task

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+// const list = document.querySelector('#categories');
+// console.dir(list);
+// console.log([...list.children]);
+
+// const listStatic = document.querySelectorAll('li.item');
+// const listDynamic = document.getElementsByClassName('item');
+// const list = document.querySelector('#categories');
+// console.log(listStatic);
+// console.log(listDynamic);
+
+const list = document.querySelector('#categories');
+// console.dir(list);
+
+const items = list.querySelectorAll('li.item');
+console.log('Number of categories:', items.length);
+
+items.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const nestedItems = item.querySelectorAll('li');
+
+  console.log('Category:', title);
+  console.log('Elements:', nestedItems.length);
+});
